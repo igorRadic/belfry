748c3a8a349e9ae67c02bb33854f2da9d9129c46
Igor Radić <igor.radic3vu@gmail.com>
2023-05-17 17:42:57 +0200

    added bell programs

--- bells.py
+++ bells.py
@@ -14,14 +14,114 @@ Bell A is the largest, bell D is the smallest
 """
 
 bell_programs = [
+    # Every day
     {
-        "name": "Nedjelja: 15 do mise",
-        "bells": [BELL_A, BELL_B],
+        "name": "Svaki dan: 12h",
+        "bells": [BELL_A, BELL_B, BELL_C, BELL_D],
+        "hour": [12],  # start hour
+        "minute": [00],  # start minute
+        "day": [0, 1, 2, 3, 4, 5, 6],  # Every day
+        "duration": 120,  # [s]
+    },
+    {
+        "name": "Svaki dan: 20h",
+        "bells": [BELL_A, BELL_D],
+        "hour": [20],  # start hour
+        "minute": [00],  # start minute
+        "day": [0, 1, 2, 3, 4, 5, 6],  # Every day
+        "duration": 120,  # [s]
+    },
+    # Work day
+    {
+        "name": "Radni dan: 15 do večernje mise",
+        "bells": [BELL_C],
+        "hour": [18],  # start hour
+        "minute": [15],  # start minute
+        "day": [0, 1, 2, 3, 4, 5],  # Work day
+        "duration": 120,  # [s]
+    },
+    # Sunday
+    {
+        "name": "Nedjelja: 15 do jutarnje mise",
+        "bells": [BELL_C],
         "hour": [6],  # start hour
         "minute": [45],  # start minute
-        "day": [6],  # sunday
+        "day": [6],  # Sunday
         "duration": 120,  # [s]
-    }
+    },
+    {
+        "name": "Nedjelja: 5 do jutarnje mise",
+        "bells": [BELL_B, BELL_C],
+        "hour": [6],  # start hour
+        "minute": [55],  # start minute
+        "day": [6],  # Sunday
+        "duration": 120,  # [s]
+    },
+    {
+        "name": "Nedjelja: 15 do župne mise",
+        "bells": [BELL_C],
+        "hour": [9],  # start hour
+        "minute": [45],  # start minute
+        "day": [6],  # Sunday
+        "duration": 120,  # [s]
+    },
+    {
+        "name": "Nedjelja: 5 do župne mise",
+        "bells": [BELL_B, BELL_C],
+        "hour": [9],  # start hour
+        "minute": [55],  # start minute
+        "day": [6],  # Sunday
+        "duration": 120,  # [s]
+    },
+    {
+        "name": "Nedjelja: 15 do večernje mise",
+        "bells": [BELL_C],
+        "hour": [18],  # start hour
+        "minute": [15],  # start minute
+        "day": [6],  # Sunday
+        "duration": 120,  # [s]
+    },
+    {
+        "name": "Nedjelja: 5 do večernje mise",
+        "bells": [BELL_B, BELL_C],
+        "hour": [18],  # start hour
+        "minute": [25],  # start minute
+        "day": [6],  # Sunday
+        "duration": 120,  # [s]
+    },
+    # Other
+    {
+        "name": "Sprovod 12:30",
+        "bells": [BELL_A, BELL_B, BELL_C, BELL_D],
+        "hour": [12],  # start hour
+        "minute": [30],  # start minute
+        "day": [],  # Only when activated
+        "duration": 120,  # [s]
+    },
+    {
+        "name": "Sprovod 12:55",
+        "bells": [BELL_A, BELL_B, BELL_C, BELL_D],
+        "hour": [12],  # start hour
+        "minute": [55],  # start minute
+        "day": [],  # Only when activated
+        "duration": 120,  # [s]
+    },
+    {
+        "name": "Sprovod 14:30",
+        "bells": [BELL_A, BELL_B, BELL_C, BELL_D],
+        "hour": [14],  # start hour
+        "minute": [30],  # start minute
+        "day": [],  # Only when activated
+        "duration": 120,  # [s]
+    },
+    {
+        "name": "Sprovod 14:55",
+        "bells": [BELL_A, BELL_B, BELL_C, BELL_D],
+        "hour": [14],  # start hour
+        "minute": [55],  # start minute
+        "day": [],  # Only when activated
+        "duration": 120,  # [s]
+    },
 ]
 
 # set up the GPIO modes
