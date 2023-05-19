7f594db645c2f994c7561b49080e04a9cfd9e8f1
Igor Radić <igor.radic3vu@gmail.com>
2023-05-19 22:52:41 +0200

    added program states persistance

--- function_buttons.py
+++ function_buttons.py
@@ -42,6 +42,49 @@ def is_pressed(previous_pressed: bool, button: int) -> bool:
         return False
 
 
+def log_function_buttons_states(
+    function_buttons_states: list[bool], current_datetime: datetime.datetime
+):
+    """
+    Log function buttons states change in text file.
+    """
+    old_states, old_states_datetime = get_function_buttons_states()
+    with open("function_buttons.txt", "w") as txt_file:
+        for state, old_state, old_datetime in zip(
+            function_buttons_states, old_states, old_states_datetime
+        ):
+            if state != old_state:
+                txt_file.writelines(
+                    f"{state} {current_datetime.strftime('%d/%m/%y %H:%M:%S')} \n"
+                )
+            else:
+                txt_file.writelines(
+                    f"{old_state} {old_datetime.strftime('%d/%m/%y %H:%M:%S')} \n"
+                )
+
+
+def get_function_buttons_states():
+    """
+    Get last function buttons states from text file.
+    """
+    states = []
+    states_datetime = []
+    with open("function_buttons.txt", "r") as txt_file:
+        lines = txt_file.readlines()
+        for line in lines:
+            [state, state_date, state_time, _] = line.split(" ")
+            state_datetime_string = state_date + " " + state_time
+            state_datetime = datetime.datetime.strptime(
+                state_datetime_string, "%d/%m/%y %H:%M:%S"
+            )
+            if state == "True":
+                states.append(True)
+            else:
+                states.append(False)
+            states_datetime.append(state_datetime)
+    return states, states_datetime
+
+
 def function_buttons(states_queue: Queue, current_datetime_queue: Queue):
     """This is main function for buttons.
 
@@ -52,6 +95,26 @@ def function_buttons(states_queue: Queue, current_datetime_queue: Queue):
     buttons_state = [False, False]
     program_states = [False, False]
 
+    # Set funciton elapse datetime.
+    states_datetime = [
+        datetime.datetime.now().replace(hour=13, minute=0, second=0),
+        datetime.datetime.now().replace(hour=15, minute=0, second=0),
+    ]
+
+    for i in range(len(states_datetime)):
+        if datetime.datetime.now() > states_datetime[i]:
+            states_datetime[i] = states_datetime[i] + datetime.timedelta(days=1)
+
+    # Check for logged program states.
+    logged_states, logged_states_datetime = get_function_buttons_states()
+
+    # Set current program states to logged program states.
+    for i in range(len(program_states)):
+        # Update program state only if logged state not expired.
+        delta = states_datetime[i] - logged_states_datetime[i]
+        if int(delta.total_seconds() / (60 * 60)) < 24:
+            program_states[i] = logged_states[i]
+
     while True:
         # Get current buttons states.
         for i, button, state in zip(range(len(buttons)), buttons, buttons_state):
@@ -61,6 +124,10 @@ def function_buttons(states_queue: Queue, current_datetime_queue: Queue):
                 program_states[i] = not program_states[i]
                 # Send new program states to other processes.
                 states_queue.put(program_states)
+                log_function_buttons_states(
+                    current_datetime=datetime.datetime.now(),
+                    function_buttons_states=program_states,
+                )
 
         # Debounce delay.
         time.sleep(0.1)
@@ -83,6 +150,10 @@ def function_buttons(states_queue: Queue, current_datetime_queue: Queue):
                 program_states[0] = False
                 # Send new program states to other processes.
                 states_queue.put(program_states)
+                log_function_buttons_states(
+                    current_datetime=datetime.datetime.now(),
+                    function_buttons_states=program_states,
+                )
             elif (
                 program_states[1]
                 and current_datetime.hour == 15
@@ -93,6 +164,10 @@ def function_buttons(states_queue: Queue, current_datetime_queue: Queue):
                 program_states[1] = False
                 # Send new program states to other processes.
                 states_queue.put(program_states)
+                log_function_buttons_states(
+                    current_datetime=datetime.datetime.now(),
+                    function_buttons_states=program_states,
+                )
 
         # Set LEDs states based on program states.
         for led, state in zip(leds, program_states):
